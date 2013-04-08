ff81252147155de313e8ca05718cd0f83d4ba947
captconrado <ottey001@gmail.com>
2013-04-07 23:42:41 -0700

    cleaned up file

--- resume.rb
+++ resume.rb
@@ -3,30 +3,14 @@ require 'prawn'
 class Resume
 	attr_accessor :user_name, :file_name
 
-def initialize(options={})
-	self.user_name = options[:user_name] || "Your Name"
-	self.file_name = options[:file_name] || "xyz.pdf"
-	# self.average_prose = 300
-	# self.average_screen = 180
-	# self.book = open(self.url).read
-
-	# parse_book
-end
-
-
-	
-	def title
-
+	def initialize(options={})
+		self.user_name = options[:user_name] || "Your Name"
+		self.file_name = options[:file_name] || "resume.pdf"
 	end
-
 
 	def self.rspec_works
 		true
 	end
-
-
-
-
 
 end
 
